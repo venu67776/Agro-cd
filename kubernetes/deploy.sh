56a0783f97abc7747f2671119a0fc9e4a8ab51e9
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-12-16 11:40:33 +0530

    new

---
+++ kubernetes/deploy.sh
@@ -0,0 +1,6 @@
+kubectl apply -f frontenddeployment.yml
+kubectl apply -f frontend-service.yml
+kubectl apply -f backenddeployment.yml
+kubectl apply -f backend-service.yml
+kubectl apply -f mysqldeployment.yml
+kubectl apply -f mysql-service.yml
